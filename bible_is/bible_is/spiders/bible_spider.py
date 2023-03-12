428a5fd128c4b5837679f537088c8e4242370f51
Khang Vo <nhatkhangdtp@gmail.com>
2023-03-12 20:47:08 +0700

    Pulling now

--- bible_is/bible_is/spiders/bible_spider.py
+++ bible_is/bible_is/spiders/bible_spider.py
@@ -1,13 +1,34 @@
-import scrapy
+import requests
+import time
+from bs4 import BeautifulSoup
 
-class BibleSpider(scrapy.Spider):
-    name = 'bible'
-    allowed_domains = ['live.bible.is']
-    start_urls = ['http://live.bible.is/bible/BDQDVS/MAT/1']
+url = 'http://live.bible.is/bible/BDQDVS/MAT/1'
 
-    def parse(self, response):
-        verses = response.css('.verse')
-        for verse in verses:
-            verse_number = verse.css('.verseNumber::text').get()
-            verse_text = verse.css('.verseText::text').get()
-            print(f"{verse_number}: {verse_text}")
+while True:
+    # Set user agent and proxy
+    headers = {
+        'User-Agent': 'Bot-1.0'
+    }
+    proxies = {
+        'http': 'http://123.45.67.89:8080',
+        'https': 'https://123.45.67.89:8080'
+    }
+
+    # Send request and parse response
+    response = requests.get(url, headers=headers, proxies=proxies)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    verses = soup.select('.chapter-justify')
+    for verse in verses:
+        verse_number = verse.select_one('.verseNumber').text.strip()
+        verse_text = verse.select_one('.verseText').text.strip()
+        print(f"{verse_number}: {verse_text}")
+
+    # Change user agent and proxy every 30 seconds
+    time.sleep(30)
+    headers = {
+        'User-Agent': 'Bot-2.0'
+    }
+    proxies = {
+        'http': 'http://12.34.56.78:8080',
+        'https': 'https://12.34.56.78:8080'
+    }
